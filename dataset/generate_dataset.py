83328a7825b9506dda919ba938d59db337dba5a5
emondsarker <emon331@gmail.com>
2025-02-05 17:52:06 -0800

    feat: add flag to generated sorted data for binary search

--- dataset/generate_dataset.py
+++ dataset/generate_dataset.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import uuid
+import argparse
 from dataset_limit import DATASET_LIMIT
 
 def generate_username():
@@ -8,13 +9,27 @@ def generate_username():
 
     return username
 
-def generate_dataset():
-    with open('./dataset.txt', 'w') as f:
-        for _ in range(DATASET_LIMIT):
-            f.write(generate_username() + '\n')
-            if _ % 1000 == 0:
-                print(f"Generated {_} usernames.")
+def generate_dataset(sorted_flag):
+    dataset_filename = './dataset.txt'
+    usernames = []
+    for _ in range(DATASET_LIMIT):
+        usernames.append(generate_username())
+        if _ % 1000 == 0:
+            print(f"Generated {_} usernames.")
+    
+    if sorted_flag:
+        usernames.sort()
+        dataset_filename = './sorted_dataset.txt'
+    
+    with open(dataset_filename, 'w') as f:
+        for username in usernames:
+            f.write(username + '\n')
     
     print(f"Generated {DATASET_LIMIT} usernames.")
 
-generate_dataset()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate a dataset of usernames.')
+    parser.add_argument('--sorted', action='store_true', help='Generate sorted usernames')
+    args = parser.parse_args()
+    
+    generate_dataset(args.sorted)
