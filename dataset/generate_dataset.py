6e9973fdedb267d7b684d55b5eae82a0e3640dea
emondsarker <emon331@gmail.com>
2025-02-05 17:28:30 -0800

    feat: update dataset limit and generator

--- dataset/generate_dataset.py
+++ dataset/generate_dataset.py
@@ -5,7 +5,6 @@ from dataset_limit import DATASET_LIMIT
 def generate_username():
     fake = Faker()
     username = fake.first_name() + fake.last_name() + str(uuid.uuid4())
-    print(username)
 
     return username
 
@@ -13,5 +12,9 @@ def generate_dataset():
     with open('./dataset.txt', 'w') as f:
         for _ in range(DATASET_LIMIT):
             f.write(generate_username() + '\n')
+            if _ % 1000 == 0:
+                print(f"Generated {_} usernames.")
+    
+    print(f"Generated {DATASET_LIMIT} usernames.")
 
 generate_dataset()
