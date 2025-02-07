3271708f25cff57dcccfd7233aa55099c2dc55fe
emondsarker <emon331@gmail.com>
2025-02-07 00:23:38 -0800

    feat: add runtime analysis plotter for Bloom Filter performance

---
+++ plotter/plot_bloom_filter.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import random
+import time
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from algorithms.bloom_filter import BloomFilter
+from plotter import Plotter
+from dataset.dataset_constants import DATASET_LIMIT, DATASET_STEP
+
+n_values = []
+runtime_values = []
+
+for i in range(DATASET_STEP, DATASET_LIMIT, DATASET_STEP):
+    with open('dataset.txt', 'r') as file:
+        lines = [line.strip() for line in file.readlines()]
+        print(i)
+        dataset = lines[:i]
+        if(len(dataset) == 0):
+            raise Exception("Dataset is empty.")
+        
+        filter = BloomFilter(dataset)
+        
+        times = []
+        for _ in range(10):
+            target = dataset[random.randint(0, len(dataset) - 1)]
+            start_time = time.time()
+            result = filter.search(target)
+            end_time = time.time()
+            run_time = end_time - start_time
+            times.append(run_time)        
+        
+        run_time = sum(times) / len(times)
+        n_values.append(len(dataset))
+        runtime_values.append(run_time)
+        
+        print(f"Test Bloom Filter: n={len(dataset)}, target={target}, result={result}, runtime={run_time:.6f}s")
+
+plotter = Plotter('runtime_analysis')
+plotter.generate_line_graph(
+    'N / Number of Login Names', 
+    n_values, 
+    'Runtime value / seconds',
+    runtime_values, 
+    'bloom_filter'
+)
